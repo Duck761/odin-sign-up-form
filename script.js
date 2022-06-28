921c9eda2531f7c4ab8a1eab867fd8bb0644da64
Dylan Mesho <106352735+Duck761@users.noreply.github.com>
2022-06-28 02:56:19 -0400

    Finished setting up the project

---
+++ script.js
@@ -0,0 +1,10 @@
+// This project is licensed under the MIT license.
+// Copyright (c) 2018-2019, David H.
+// All rights reserved.
+// ------------------------------------------------------------
+
+// This project did not require any JS.
+// It is just a simple HTML page with a simple CSS style.
+// It is used to show the usage of the "CSS forms" feature.
+// The CSS style is in the file "style.css".
+// The HTML page is in the file "index.html".
