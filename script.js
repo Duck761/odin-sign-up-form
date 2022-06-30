124e1641da2c7a45e7a4246349eb8b97f75a43f7
Dylan Mesho <106352735+Duck761@users.noreply.github.com>
2022-06-29 20:16:31 -0400

    Form validity check complete

--- script.js
+++ script.js
@@ -8,3 +8,25 @@
 // It is used to show the usage of the "CSS forms" feature.
 // The CSS style is in the file "style.css".
 // The HTML page is in the file "index.html".
+
+const firstName = document.getElementById("first-name");
+const lastName = document.getElementById("last-name");
+const email = document.getElementById("email");
+const phoneNumber = document.getElementById("number");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirm-password");
+const submit = document.getElementById("create-account");
+const allForms = [firstName, lastName, email, phoneNumber, password, confirmPassword];
+
+submit.addEventListener('click', () => {
+    if (allForms.every(form => form.checkValidity())) {
+        if (password.value !== confirmPassword.value) {
+            alert("Passwords do not match!");
+        } else {
+            alert("Account created!");
+        }
+    } else {
+        alert("Please fill out all fields!");
+    }
+});
+
